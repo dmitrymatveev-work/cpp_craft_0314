1ed9b2edf47c995fbe2999e859ffe88c34e318bc
Dmitry Matveev <dmitrymatveev.work@gmail.com>
2014-03-23 22:59:11 +0300

    The 2nd task done. Also I modified task 1_3 to use BFS (now named as 1_3_BFS)

--- solutions/dmitry_matveev/2/sources/2_6/binary_datafeed.cpp
+++ solutions/dmitry_matveev/2/sources/2_6/binary_datafeed.cpp
@@ -1,7 +1,66 @@
 
 #include "binary_datafeed.h"
 
+using namespace boost::filesystem;
+
 int main()
 {
+	path inPath(BINARY_DIR "/2.6_example.in");
+	path outPath(BINARY_DIR "/2.6_example.out");
+	if(exists(inPath) && is_regular_file(inPath))
+	{
+		ifstream input(inPath, std::ios_base::binary);
+		ofstream output(outPath, std::ios_base::binary);
+
+		if(input.is_open() && output.is_open())
+		{
+			auto fSize = file_size(inPath);
+
+			while (!input.eof() && (fSize - input.tellg() >= 8 + 8 + 7*sizeof(double) + sizeof(unsigned int)))
+			{
+				boost::shared_array<char> stockName(new char[9]);
+
+				input.read(stockName.get(), 8);
+
+				unsigned int dateTime(0);
+
+				size_t yearStringLength = 4;
+				boost::shared_array<char> year(new char[yearStringLength]);
+				input.read(year.get(), yearStringLength);
+				dateTime = 372 * std::stoi(year.get(), &yearStringLength);
+
+				size_t monthStringLength = 2;
+				boost::shared_array<char> month(new char[monthStringLength]);
+				input.read(month.get(), monthStringLength);
+				dateTime += 31 * std::stoi(month.get(), &monthStringLength);
+
+				size_t dayStringLength = 2;
+				boost::shared_array<char> day(new char[dayStringLength]);
+				input.read(day.get(), dayStringLength);
+				dateTime += std::stoi(day.get(), &dayStringLength);
+
+				input.seekg((int)input.tellg() + sizeof(double));
+
+				double price(0.0);
+				input.read((char*)&price, sizeof(price));
+
+				unsigned int volume(0);
+				input.read((char*)&volume, sizeof(volume));
+
+				input.seekg((int)input.tellg() + 2 * sizeof(double));
+
+				double f2(0.0);
+				input.read((char*)&f2, sizeof(f2));
+
+				input.seekg((int)input.tellg() + 2 * sizeof(double));
+
+				output.write(stockName.get(), 9);
+				output.write((char*)&dateTime, sizeof(dateTime));
+				output.write((char*)&price, sizeof(price));
+				output.write((char*)&volume, sizeof(volume));
+				output.write((char*)&f2, sizeof(f2));
+			}
+		}
+	}
 }
 
--- solutions/dmitry_matveev/2/sources/2_6/binary_datafeed.h
+++ solutions/dmitry_matveev/2/sources/2_6/binary_datafeed.h
@@ -2,4 +2,9 @@
 #ifndef BINARY_DATAFEED
 #define BINARY_DATAFEED
 
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/shared_array.hpp>
+#include <boost/container/map.hpp>
+
 #endif
